563b449a1e83e0d6f6bd3ffe56e83468a04f62ef
Denis Davidyuk <denis_davidyuk@hotmail.com>
2021-06-13 15:07:17 +0300

    Initial implementation

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,40 @@
 module Main where
 
 import Lib
+import System.IO( stdin, hReady, hSetEcho, hSetBuffering, BufferMode( NoBuffering ) )
+import System.Timeout( timeout )
+import Data.Maybe( fromMaybe )
 
-main :: IO ()
-main = someFunc
+main :: IO()
+main = do
+    hSetBuffering stdin NoBuffering
+    hSetEcho stdin False
+    snakeHead <- randomPoint
+    food <- randomPoint
+    putStr "\ESC[2J"
+    putStrLn (getField [snakeHead] food)
+    action <- readAction
+    loop [snakeHead] action food
+    putStrLn "Game over"
+
+loop :: [(Int, Int)] -> Action -> (Int, Int) -> IO()
+loop snake action food = do
+    putStr "\ESC[2J"
+    let time = 150000 + round(250000 * maximum [fromIntegral (11 - length snake) / 10, 0])
+    nextActionMaybe <- timeout time readAction
+    let nextActionIndirect = fromMaybe action nextActionMaybe
+    let nextAction = if length snake /= 1 && movePoint (snake!!0) nextActionIndirect == snake!!1 then action else nextActionIndirect
+
+    let nextSnakeHead = movePoint (snake!!0) nextAction
+    let ate = food == nextSnakeHead
+    let nextSnake = nextSnakeHead : if ate then snake else init snake
+    nextFoodRandom <- randomPoint
+    let nextFood = if ate then nextFoodRandom else food
+
+    if isSnakeValid nextSnake
+        then do
+            putStrLn ("Snake length: " ++ (show (length snake)))
+            putStrLn (getField nextSnake nextFood)
+            loop nextSnake nextAction nextFood
+        else
+            return ()
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,62 @@
 module Lib
-    ( someFunc
+    ( getField,
+      readAction,
+      movePoint,
+      isSnakeValid,
+      randomPoint,
+      includes,
+      Action(..)
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.List( elemIndex, find, nub )
+import Data.Ix( inRange )
+import System.Random( randomIO )
+fieldSize = [40, 15]
+
+includes :: Eq a => a -> [a] -> Bool
+includes el list = (elemIndex el list) /= Nothing
+
+isSnakeValid :: [(Int, Int)] -> Bool
+isSnakeValid snake =
+    (find (\(x, y) -> not (inRange (0, fieldSize!!0 - 1) x && inRange (0, fieldSize!!1 - 1) y)) snake) == Nothing
+    && nub snake == snake
+
+randomPoint :: IO((Int, Int))
+randomPoint = do
+    x <- randomIO :: IO Int
+    y <- randomIO :: IO Int
+    return (mod x (fieldSize!!0), mod y (fieldSize!!1))
+
+getFieldChar :: [(Int, Int)] -> (Int, Int) -> (Int, Int) -> Char
+getFieldChar snake food point =
+    if includes point snake then '*' else if point == food then '+' else ' '
+
+getField :: [(Int, Int)] -> (Int, Int) -> String
+getField snake food =
+    unlines (
+        [(replicate (fieldSize!!0 + 2) '_')] ++
+        (map
+            (\y -> ("|" ++ (map (\x -> getFieldChar snake food (x, y)) [0..(fieldSize!!0 - 1)]) ++ "|"))
+            [0..(fieldSize!!1 - 1)])
+        ++ [(replicate (fieldSize!!0 + 2) '-')]
+    )
+
+data Action = TurnUp | TurnDown | TurnLeft | TurnRight
+    deriving (Show, Read, Eq, Ord)
+
+readAction :: IO(Action)
+readAction = do
+    char <- getChar
+    case char of
+        'w' -> return TurnUp
+        's' -> return TurnDown
+        'd' -> return TurnRight
+        'a' -> return TurnLeft
+        _ -> undefined
+
+movePoint :: (Int, Int) -> Action -> (Int, Int)
+movePoint (x, y) action = case action of
+    TurnUp -> (x, y - 1)
+    TurnDown -> (x, y + 1)
+    TurnRight -> (x + 1, y)
+    TurnLeft -> (x - 1, y)
